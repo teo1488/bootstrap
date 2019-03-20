c4ead90b242146c3f31688e0e95c5e6d3e5d65b7
teo1488 <litvm1274@gmail.com>
2019-03-20 22:05:13 +0200

    first commot

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// var message="Hello";
+// var message2="World";
+// console.log(message);
+// console.log(message2);
+// console.log(message2 + " " + message);
+var k=5;
+var s="str";
+var b=true;
+var n=10;
+var summ= k+n;
+console.log("Сумма: "+ summ);
+var m=k-n;
+console.log("Вычитание: "+ m);
+var d=k/n;
+console.log("Деление "+ d);
+var z=k*n;
+console.log("Умножение: "+ z);
+var e=Math.sqrt(64,3);
+console.log("Возведение в степень: "+ e)
+
